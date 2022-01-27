adc5e16c5a83b0bb02571e4f57d136b76501518d
SelimSevgi <slmsvg44@gmail.com>
2022-01-27 16:32:50 +0300

    data import script

---
+++ backend/data/products.js
@@ -0,0 +1,80 @@
+const products = [
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/41/550/10711870603314.jpg/format:webp",
+    description: "Sony Playstation 5 Oyun Konsolu (Eurasia garantili)",
+    price: 13.69,
+    countInStock: 500,
+  },
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/41/550/10711870603314.jpg/format:webp",
+    description: "Sony Playstation 5 Oyun Konsolu (Eurasia garantili)",
+    price: 13.69,
+    countInStock: 500,
+  },
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/41/550/10711870603314.jpg/format:webp",
+    description: "Sony Playstation 5 Oyun Konsolu (Eurasia garantili)",
+    price: 13.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+  {
+    name: "iphone 13",
+    imageUrl:
+      "https://productimages.hepsiburada.net/s/119/550/110000068435066.jpg/format:webp",
+    description:
+      "The iPhone 13 and iPhone 13 mini iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch has been reduced in size, and the rear camera module now sits at a diagonal. In addition, the A15 processor brings more speed and efficiency to every task.",
+    price: 15.699,
+    countInStock: 500,
+  },
+];
